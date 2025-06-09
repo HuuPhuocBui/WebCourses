87a5f3f4ffb0cd82b5575af31159a82fa75544d9
HuuPhuocBui <135402077+HuuPhuocBui@users.noreply.github.com>
2025-06-09 08:32:20 +0700

    9/6/2025

--- Server/src/route/course.js
+++ Server/src/route/course.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllCourses, getCourseById } = require("../controller/course");
-
 const { getAllCourses, getCourseById, getCourseContentById} = require("../controller/course");
 
 
